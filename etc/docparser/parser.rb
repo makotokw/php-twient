aeb98cafeea28a2975b6e554c1e47d4007f73ca5
makoto_kw <makoto.kw@gmail.com>
2013-03-02 02:34:02 +0900

    Fixed document parser

--- etc/docparser/parser.rb
+++ etc/docparser/parser.rb
@@ -13,14 +13,14 @@ rest_api_doc_template = ERB.new(<<"EOS"
 /**
  * @param array $params
  * @return array
-<% api['url'].each do |url| %> * @see <%= url %>
+<% api[:url].each do |url| %> * @see <%= url %>
 <% end %> */
 EOS
 )
 rest_api_template = ERB.new(<<"EOS"
 public function <%= method_name %>($params = array())
 {
-  return $this-><%= api['http_method'][0] %>('<%= api['name'] %>', $params);
+  return $this-><%= api[:http_method][0] %>('<%= api[:name] %>', $params);
 }
 
 EOS
@@ -29,9 +29,9 @@ rest_rw_api_template = ERB.new(<<"EOS"
 public function <%= method_name %>($params = array())
 {
     if (empty($params)) {
-        return $this->get('<%= api['name'] %>', $params);
+        return $this->get('<%= api[:name] %>', $params);
     } else {
-        return $this->post('<%= api['name'] %>', $params);
+        return $this->post('<%= api[:name] %>', $params);
     }
 }
 
@@ -42,17 +42,17 @@ streaming_api_template = ERB.new(<<"EOS"
  * @param array $params
  * @param $callback
  * @return array
-<% api['url'].each do |url| %> * @see <%= url %><% end %>
+<% api[:url].each do |url| %> * @see <%= url %><% end %>
  */
 public function <%= method_name %>($params = array(), $callback)
 {
-  return $this->streaming('<%= api['name'] %>', $params, $callback);
+  return $this->streaming('<%= api[:name] %>', $params, $callback);
 }
 
 EOS
 )
 
-apis = {}
+api_items = {}
 base_url = api_doc.css('meta[@name = "twitter:url"]').attr('content')
 
 api_doc.css('.views-field-title > a').each do |link|
@@ -64,28 +64,28 @@ api_doc.css('.views-field-title > a').each do |link|
   method_name = api_name.gsub(/\//, '_').gsub(':', '')
   api_url = URI.join(base_url, link.attr('href'))
 
-  if apis.has_key?(method_name)
-    apis[method_name]['http_method'].push(http_method)
-    apis[method_name]['url'].push(api_url)
+  if api_items.has_key?(method_name)
+    api_items[method_name][:http_method].push(http_method)
+    api_items[method_name][:url].push(api_url)
   else
-    apis[method_name] = {
-        'http_method' => [http_method],
-        'name' => api_name,
-        'url' => [api_url]
+    api_items[method_name] = {
+        http_method: [http_method],
+        name: api_name,
+        url: [api_url]
     }
   end
 end
 
 post_api_names = []
-apis.each do |method_name, api|
-  if /^(statuses\/filter|statuses\/sample|statuses\/firehose|user|site)$/ =~ api['name']
-    method_name = ('streaming_' + method_name).camelize(:lower)
+api_items.each do |method_name, api|
+  if /^(?:statuses\/filter|statuses\/sample|statuses\/firehose|user|site)$/ =~ api[:name]
+    method_name = ("streaming_#{method_name}").camelize(:lower)
     puts streaming_api_template.result(binding)
   else
-    post_api_names.push(api['name']) if api['http_method'].include?('post')
+    post_api_names.push(api[:name]) if api[:http_method].include?('post')
     method_name = method_name.camelize(:lower)
     puts rest_api_doc_template.result(binding)
-    puts (api['url'].size > 1) ? rest_rw_api_template.result(binding) : rest_api_template.result(binding)
+    puts (api[:url].size > 1) ? rest_rw_api_template.result(binding) : rest_api_template.result(binding)
   end
 end
 
